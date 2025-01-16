d4091845756315e077d6985a7467d0369270100c
Josef-Novinski <109561791+Josef-Novinski@users.noreply.github.com>
2025-01-15 19:05:36 -0600

    hmm

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -4,7 +4,7 @@ var config_data = `
   "title": "Scouting PASS 2025",
   "page_title": "REEFSCAPE",
   "checkboxAs": "10",
-  "PREmatch": [
+  "prematch": [
     { "name": "Scouter Initials",
       "code": "s",
       "type": "scouter",

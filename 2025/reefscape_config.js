8f945904c39804ed85b8a2446e878ee3f16c7f6b
Josef-Novinski <109561791+Josef-Novinski@users.noreply.github.com>
2025-01-18 14:51:47 -0600

    hg

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -5,7 +5,7 @@ var config_data = `
   "page_title": "REEFSCAPE",
   "checkboxAs": "10",
   "prematch": [
-    { "name": "Scouter Name",
+    { "name": "Scouter",
       "code": "s",
       "type": "scouter",
       "size": 5,

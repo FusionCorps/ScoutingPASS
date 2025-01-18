dfec1eab23c21ccb8f18bd75dfd371a82e44d466
Josef-Novinski <109561791+Josef-Novinski@users.noreply.github.com>
2025-01-18 14:49:50 -0600

    authkey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "Jl8DfkXmeAwqtcjbeR5iTn4FhQ4rL2vdzI07UtSyIVx0iTc3l5EmOeSVGADRZ0s8";
 /**
  * Get list of teams in event
  *
